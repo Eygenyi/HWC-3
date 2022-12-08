f28ddd63a7824dc13b5324fb5cd215bcfce94adc
Evgeniy <freemen0083@gmail.com>
2022-12-08 22:32:00 +0400

    #2

---
+++ Program (2).cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Input X1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input X2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double Vector()
+{
+double res=Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+return res;
+}
+Console.WriteLine("Distance="+Vector());
+ 
