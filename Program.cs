da52a32f18805def9bd5c428bf1a0e31d01c29a4
Evgeniy <freemen0083@gmail.com>
2022-12-08 21:42:52 +0400

    #1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input any number");
+int num = Convert.ToInt32(Console.ReadLine());
+void Cubed()
+{
+for (int i=1;i<=num;i++)
+{
+    Console.WriteLine(i+"^3="+Math.Pow(i,3));
+}
+}
+Cubed();
