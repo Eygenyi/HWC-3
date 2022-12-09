5c1964b4110b920a9298bbc6503078803b3f8132
Evgeniy <freemen0083@gmail.com>
2022-12-09 11:26:12 +0400

    #3

---
+++ Program (3).cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Input number");
+int n = Convert.ToInt32(Console.ReadLine());
+int t=n;
+int rev=0;
+int Palindrome()
+{
+    while (n>0 || n<0)
+{
+   rev = rev * 10 + n % 10;
+    n = n / 10;
+}
+Console.WriteLine(rev);
+return rev;
+}
+
+Palindrome();
+if (t==rev)
+Console.WriteLine("is Palindrome");
+else
+Console.WriteLine("not Palindrome");
+
+
+
+ 
+
+
